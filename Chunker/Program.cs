a46b449c2fd1a15d3047cdb4f0a6101d1a9e3380
nfowler <nrfowler@gmail.com>
2019-06-04 17:10:59 -0500

    demo: concatenation algorithm

--- Chunker/Program.cs
+++ Chunker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 namespace Chunker
 {
     class Program
@@ -12,8 +13,28 @@ namespace Chunker
 
         static void Main(string[] args)
         {
-            ChunkAndRead();
+            RegExDemo(args);
 
+            //ChunkAndRead();
+
+        }
+        private const string MatchSuccess = "{0} @{1}:{2}";
+
+        private static void RegExDemo(string[] args)
+        {
+            var pattern = args[0];
+            var subject = args[1];
+            var regex = new Regex(pattern);
+            var match = regex.Match(subject);
+            if (match.Success)
+            {
+                Console.WriteLine(MatchSuccess, match.Success, match.Index, match.Length);
+            }
+            else
+            {
+                Console.WriteLine(match.Success);
+            }
+            Console.ReadLine();
         }
 
         private static void ChunkAndRead()
